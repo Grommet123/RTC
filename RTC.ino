091eed69e328bd7ff1f4050525d6a7af98f1896f
Gary Grotsky <gkgrotsky@aol.com>
2016-03-29 17:55:02 -0400

    Added files via upload

--- RTC.ino
+++ RTC.ino
@@ -7,41 +7,14 @@
   A work in progress.
 
 */
-//#define DEBUG  // Uncomment to turn on debug
 
-#define  DHT11_PRESENT
-
+#include "RTC.h"
 #include <Wire.h>
 #include "ds3231.h"
 #include <LiquidCrystal.h>
 #include <dht.h>
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-#define VERSION                 "V2.2" // Add temperature and humidity offset
-#define TEMPERATURE_OFFSET       3.0   // DHT11 reads high (Cheep Chinese garbage)
-#define HUMIDITY_OFFSET          6.0   // DHT11 reads low         "
-#define BUFF_MAX 128
-#define BUTTON_ADC_PIN           A0  // A0 is the button ADC input
-#define TEMP_HUM_PIN             A1  // A1 is the button humidity input
-#define LCD_BACKLIGHT_PIN        10  // D10 controls LCD back light
-// ADC readings expected for the 5 buttons on the ADC input
-#define RIGHT_10BIT_ADC           0  // right
-#define UP_10BIT_ADC            131  // up
-#define DOWN_10BIT_ADC          307  // down
-#define LEFT_10BIT_ADC          480  // left
-#define SELECT_10BIT_ADC        720  // select
-#define BUTTONHYSTERESIS         10  // hysteresis for valid button sensing window
-//return values for ReadButtons()
-#define BUTTON_NONE               0
-#define BUTTON_RIGHT              1
-#define BUTTON_UP                 2
-#define BUTTON_DOWN               3
-#define BUTTON_LEFT               4
-#define BUTTON_SELECT             5
-// Used to check the long spelling months
-#define SEP                       9
-#define NOV                       10
-#define DEC                       12
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 uint8_t time[8];
 char recv[BUFF_MAX];
