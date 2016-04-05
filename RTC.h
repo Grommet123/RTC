cb498730fc8db131685863e40aa336054e710f12
Gary Grotsky <gkgrotsky@aol.com>
2016-04-05 18:33:36 -0400

    V3.0 Add RGB LED for temperature warning

--- RTC.h
+++ RTC.h
@@ -2,7 +2,7 @@
  * RTC.h
  *
  * Created: 3/29/2016 1:50:21 PM
- *  Author: Gary
+ *  Author: Gary Grotsky
  */ 
 
 
@@ -14,13 +14,16 @@
 
 #define  DHT11_PRESENT
 
-#define VERSION                 "V1.0" // Initial Release
+#define VERSION                 "V3.0" // Add RGB LED for temperature warning
 #define TEMPERATURE_OFFSET       1.8   // DHT11 reads high (Cheep Chinese garbage)
 #define HUMIDITY_OFFSET          6.0   // DHT11 reads low         "
 #define BUFF_MAX 128
 #define BUTTON_ADC_PIN           A0  // A0 is the button ADC input
 #define TEMP_HUM_PIN             A1  // A1 is the DHT11 input
 #define LCD_BACKLIGHT_PIN        10  // D10 controls LCD back light
+#define RED_LED                  22  // D22 is the red LED
+#define GREEN_LED                24  // D24 is the green LED
+#define BLUE_LED                 26  // D26 is the blue LED
 // ADC readings expected for the 5 buttons on the ADC input
 #define RIGHT_10BIT_ADC           0  // right
 #define UP_10BIT_ADC            131  // up
@@ -39,6 +42,9 @@
 #define SEP                       9
 #define NOV                       10
 #define DEC                       12
+// Temperature settings
+#define TEMPERATURE_TO_HIGH       78.0f
+#define TEMPERATURE_TO_LOW        65.0f
 
 void parse_cmd(char *cmd, int cmdsize);
 void printMonth(int month);
