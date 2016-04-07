0f1606511099bb43336e9b81d5263758c16f02b7
Gary Grotsky <gkgrotsky@aol.com>
2016-04-07 19:30:02 -0400

    v4.0 Flash the red and blue LEDs

--- RTC.h
+++ RTC.h
@@ -14,16 +14,16 @@
 
 #define  DHT11_PRESENT
 
-#define VERSION                 "V3.0" // Add RGB LED for temperature warning
+#define VERSION                 "V4.0" // Flash the red and blue LEDs
 #define TEMPERATURE_OFFSET       1.8   // DHT11 reads high (Cheep Chinese garbage)
 #define HUMIDITY_OFFSET          6.0   // DHT11 reads low         "
 #define BUFF_MAX 128
-#define BUTTON_ADC_PIN           A0  // A0 is the button ADC input
-#define TEMP_HUM_PIN             A1  // A1 is the DHT11 input
-#define LCD_BACKLIGHT_PIN        10  // D10 controls LCD back light
-#define RED_LED                  22  // D22 is the red LED
-#define GREEN_LED                24  // D24 is the green LED
-#define BLUE_LED                 26  // D26 is the blue LED
+#define BUTTON_ADC_PIN           A0  // The button ADC input
+#define TEMP_HUM_PIN             A1  // The DHT11 input
+#define LCD_BACKLIGHT_PIN        10  // The controls LCD back light
+#define RED_LED                  49  // The red LED
+#define GREEN_LED                51  // The green LED
+#define BLUE_LED                 53  // The blue LED
 // ADC readings expected for the 5 buttons on the ADC input
 #define RIGHT_10BIT_ADC           0  // right
 #define UP_10BIT_ADC            131  // up
@@ -44,7 +44,7 @@
 #define DEC                       12
 // Temperature settings
 #define TEMPERATURE_TO_HIGH       78.0f
-#define TEMPERATURE_TO_LOW        65.0f
+#define TEMPERATURE_TO_LOW        60.0f
 
 void parse_cmd(char *cmd, int cmdsize);
 void printMonth(int month);
